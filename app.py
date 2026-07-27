1e9437962bb9e2e2e73a1912e741228fcb324da2
Manvi Varma <manvivarma2007@gmail.com>
2026-07-27 20:28:31 +0530

    Added search page and login page

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
 @app.route("/report-lost")
 def report_lost():
     return render_template("report_lost.html")
-
 
 @app.route("/report-found")
 def report_found():
     return render_template("report_found.html")
 
-
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+@app.route("/search")
+def search():
+    return render_template("search.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

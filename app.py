884b46fa056dc90d4d15630436930dc9614fed70
Manvi Varma <manvivarma2007@gmail.com>
2026-07-27 20:11:49 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/report-lost")
+def report_lost():
+    return render_template("report_lost.html")
+
+
+@app.route("/report-found")
+def report_found():
+    return render_template("report_found.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
